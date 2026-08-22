473520904f49e3cc0b9aca335c4752bddb948040
swadhinbiswas <swadhinbiswas.cse@gmail.com>
2026-08-23 02:33:01 +0600

    Fix resize_on_border: re-enable after Omarchy looknfeel override

--- config/hypr/deo-layer.lua
+++ config/hypr/deo-layer.lua
@@ -2,6 +2,10 @@ local require_optional = require("default.hypr.require_optional")
 require("default.hypr.helpers")
 require("default.hypr.envs")
 require("default.hypr.looknfeel")
+
+-- Re-enable resize on border (Omarchy defaults to false, but we want it)
+hl.config({ general = { resize_on_border = true } })
+
 require_optional.module("omarchy.current.theme.hyprland")
 require("default.hypr.windows")
 require("hypr.deo-bindings")
